946f6a1107bece859b5240fbdb3737776f55f112
cyberdremer <73867161+cyberdremer@users.noreply.github.com>
2021-12-11 15:46:18 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.util.Scanner;
+import java.io.InterruptedIOException;import java.util.Scanner;
 
 
 public class Main {
     static BufferedImage InputImage,OutputImage;
-    static int [][] pixelMatrix = new int[3][3];
+    static int [][] kernelMatrix = new int[3][3];
+    static int [][] Gx = {{-1, 0, 1}, {-2, 0, 2 } , {-1, 0, 1}};
+    static int [][] Gy = {{1,2,1}, {0,0,0}, {-1,-2,1}};
 
     public static void main(String[] args) {
 
@@ -25,19 +26,19 @@ public class Main {
             for(int i = 1; i < InputImage.getWidth()-1; i++ ){
                 for (int j = 1; j < InputImage.getHeight()-1; j++){
 
-                    pixelMatrix[0][0] = new Color(InputImage.getRGB(i-1, j-1)).getRed();
-                    pixelMatrix[0][1] = new Color(InputImage.getRGB(i-1, j)).getRed();
-                    pixelMatrix[0][2] = new Color(InputImage.getRGB(i-1, j +1)).getRed();
-                    pixelMatrix[1][0] = new Color(InputImage.getRGB(i,j-1)).getRed();
-                    pixelMatrix[1][1] = new Color(InputImage.getRGB(i,j)).getRed();
-                    pixelMatrix[1][2] = new Color(InputImage.getRGB(i+1,j-1)).getRed();
-                    pixelMatrix[2][0] = new Color(InputImage.getRGB(i+1,j-1)).getRed();
-                    pixelMatrix[2][1] = new Color(InputImage.getRGB(i+1,j)).getRed();
-                    pixelMatrix[2][2] = new Color(InputImage.getRGB(i+1,j+1)).getRed();
+                    kernelMatrix[0][0] = new Color(InputImage.getRGB(i-1, j-1)).getRed();
+                    kernelMatrix[0][1] = new Color(InputImage.getRGB(i-1, j)).getRed();
+                    kernelMatrix[0][2] = new Color(InputImage.getRGB(i-1, j +1)).getRed();
+                    kernelMatrix[1][0] = new Color(InputImage.getRGB(i,j-1)).getRed();
+                    kernelMatrix[1][1] = new Color(InputImage.getRGB(i,j)).getRed();
+                    kernelMatrix[1][2] = new Color(InputImage.getRGB(i+1,j-1)).getRed();
+                    kernelMatrix[2][0] = new Color(InputImage.getRGB(i+1,j-1)).getRed();
+                    kernelMatrix[2][1] = new Color(InputImage.getRGB(i+1,j)).getRed();
+                    kernelMatrix[2][2] = new Color(InputImage.getRGB(i+1,j+1)).getRed();
 
                     //Take in the red value of the pixel in the image and places it into the pixel matrix
 
-                    int edge = (int) Convolution(pixelMatrix);
+                    int edge = (int) Convolution(kernelMatrix,Gx,Gy);
 
                     //Takes in the pixelMatrix and performs convolution on it using Gx and Gy kernels
 
@@ -77,16 +78,18 @@ public class Main {
 
 
 
-    public static double Convolution(int[][] pixelMatrix){
+    public static double Convolution(int[][] pixelMatrix, int[][] Gx, int[][] Gy){
 
-        int DirectionalX = (pixelMatrix[0][0] * -1) + (pixelMatrix[0][2] * 1) + (pixelMatrix[1][0] * -2) + (pixelMatrix[1][2] * 2) +
-                (pixelMatrix[2][0] * -1) + (pixelMatrix[2][2] * 1);
+        int DirectionalX = (pixelMatrix[0][0] * -Gx[0][0]) +(pixelMatrix[0][1] * Gx[0][1])+ (pixelMatrix[0][2] * Gx[0][2]) +
+                (pixelMatrix[1][0] * -Gx[1][0]) + (pixelMatrix[1][1] * Gx[1][1]) +(pixelMatrix[1][2] * Gx[1][2]) +
+                (pixelMatrix[2][0] * -Gx[2][0]) + (pixelMatrix[2][1] * Gx[2][1])+ (pixelMatrix[2][2] * Gx[2][2]);
         //Calculates the DirectionalX using the kernel for sobel operator in X direction
-        int DirectionalY = (pixelMatrix[0][0]*1) + (pixelMatrix[0][1] * 2) + (pixelMatrix[0][2] * 1) + (pixelMatrix[2][0]*-1) + (pixelMatrix[2][1]*-2)
-                +(pixelMatrix[2][2]*-1);
+        int DirectionalY = (pixelMatrix[0][0]*Gy[0][0]) + (pixelMatrix[0][1] * Gy[0][0]) + (pixelMatrix[0][2] * Gy[0][2]) +
+                (pixelMatrix[1][0] * Gy[1][0]) + (pixelMatrix[1][1] * Gy[1][1]) + (pixelMatrix[1][2] * Gy[1][2]) +
+                (pixelMatrix[2][0]* -Gy[2][0]) + (pixelMatrix[2][1]* -Gy[2][1]) +(pixelMatrix[2][2]* -Gy[2][2]);
         //Calculates the DirectionalY using the kernel for sobel operator in Y direction
 
-        return Math.sqrt(Math.pow(DirectionalX,2)+ Math.pow(DirectionalY,2)); //Normalizes the Directional Derivative in X and Y direction
+        return Math.sqrt(Math.pow(DirectionalX,2)+ Math.pow(DirectionalY,2)); //Normalizes the results so we do not obtain negative numbers as there are no negative rgb values
 
 
 
